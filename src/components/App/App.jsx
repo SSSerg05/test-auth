970fcdf0c82fa855f26f7c375d7213bafaec41ae
SSSerg05 <ssserg05@gmail.com>
2023-12-19 00:14:17 +0200

    fix
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,77 @@
-import React from "react";
-import { ClerkProvider } from "@clerk/clerk-react";
+import React, { lazy } from "react";
+import { ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  UserButton, } from "@clerk/clerk-react";
+  import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+
  
+
 const {REACT_APP_CLERK_PUBLISHABLE_KEY} = process.env;
 if (!REACT_APP_CLERK_PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-function App() {
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const LoginPage = lazy(() => import('../../pages/Sign-inPage'));
+const RegisterPage = lazy(() => import('../../pages/Sign-upPage'));
+// const ContactsPage = lazy(() => import('../../pages/ContactsPage'));
+
+function PublicPage() {
   return (
-    <ClerkProvider publishableKey={REACT_APP_CLERK_PUBLISHABLE_KEY}>
+    <>
+      <h1>Public page</h1>
+      <a href="/protected">Go to protected page</a>
+    </>
+  );
+}
+ 
+function ProtectedPage() {
+  return (
+    <>
+      <h1>Protected page</h1>
+      <UserButton />
+    </>
+  );
+}
+
+function App() {
+  const navigate = useNavigate();
+
+
+  return (
+    <ClerkProvider 
+      publishableKey={REACT_APP_CLERK_PUBLISHABLE_KEY}
+      navigate={(to) => navigate(to)}
+    >
       <div>Hello from clerk</div>
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route
+          path="/sign-in/*"
+          element={<SignIn routing="path" path="/sign-in" />}
+        />
+        <Route
+          path="/sign-up/*"
+          element={<SignUp routing="path" path="/sign-up" />}
+        />
+        <Route
+          path="/protected"
+          element={
+          <>
+            <SignedIn>
+              <ProtectedPage />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+          }
+        />
+      </Routes>
     </ClerkProvider>
   );
 }
---
+++ src/components/App/Layout.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-width: 270px;
+  margin: 0 auto;
+  padding: 0 16px;
+`;
